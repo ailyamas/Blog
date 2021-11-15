e07c3ba0806c1696bfcdc14f37cf334b84d36089
lyes <ailymas2967@gmail.com>
2021-11-15 15:53:01 +0100

    create a input search

--- Post/urls.py
+++ Post/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('detail/<int:pk>',views.detail, name="detail" ) ,
     path('addPost',views.createPost, name="add-Post" ),
     path('Update_post/<int:pk>',views.UpdatePost, name="update-Post" ) ,
-    path('Delete_post/<int:pk>',views.deletePost, name="delete-Post" ) ,
+    path('Delete_post/<int:pk>',views.deletePost, name="delete-Post" ),
+   
 ]
